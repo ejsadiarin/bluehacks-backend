ccb869dc1171cde1d979be99ed9fd69d4bd7b172
ejsadiarin <ejs@ejsadiarin.com>
2025-02-23 07:45:12 +0800

    fix: cors errors

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express()
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: "*"
+    origin: "*",
+    credentials: true,
 }))
 
 /**
