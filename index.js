099a5a4c784aaf8eb3b4309b1e0c90ca4ecc7eca
ejsadiarin <ejs@ejsadiarin.com>
2025-02-23 07:46:58 +0800

    cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
 }))
 
